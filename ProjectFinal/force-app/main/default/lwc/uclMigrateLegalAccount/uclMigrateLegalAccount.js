957926f025b9f6c2b41e419b1406b1cccd4a76f5
vsseoane <vsseoane@gmail.com>
2023-12-03 13:57:01 -0300

    Refactor
    - Change method names

--- ProjectFinal/force-app/main/default/lwc/uclMigrateLegalAccount/uclMigrateLegalAccount.js
+++ ProjectFinal/force-app/main/default/lwc/uclMigrateLegalAccount/uclMigrateLegalAccount.js
@@ -1,26 +1,30 @@
 import { LightningElement } from 'lwc';
  import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-import migrateAllLegalAccounts from '@salesforce/apex/ucl_LegalAdvisors.migrateAllLegalAccounts';
-import sendEmailNotification from '@salesforce/apex/ucl_Utils.sendEmailNotification'; 
+import migrateAllLegalAccounts from '@salesforce/apex/ucl_LegalAdvisorsService.migrateAllLegalAccounts';
+//import sendEmailNotification from '@salesforce/apex/ucl_Utils.sendEmailNotification'; 
 
 export default class UclMigrateLegalAccount extends LightningElement {
-    handleClick(event) {
+    handleClick() {
         migrateAllLegalAccounts()
             .then(response => {
+                console.log('response came: ' + response);
 
-                if(!response) throw Exception("there was an error");
-                sendEmailNotification(true, null, 'vsseoane@gmail.com');
+                if (!response || response ==  null || response === '' || response === "") {
+                   throw new Error("There was an error");
+                }
                 const toastEvent = new ShowToastEvent({
                     title: "Migration Successful",
-                    message: "The legal accounts were imported",
+                    message: "The legal accounts were imported!",
                     variant: "success"
                 });
                
-                this.dispatchEvent(toastEvent);
+               this.dispatchEvent(toastEvent);
+               // sendEmailNotification(true, null, 'vsseoane@gmail.com');
+
             })
             .catch(error => {
                 console.error('Error:', error);
-                sendEmailNotification(false, error.message, 'vsseoane@gmail.com');
+            //sendEmailNotification(false, error.message, 'vsseoane@gmail.com');
                 const toastEvent = new ShowToastEvent({
                     title: "Migration Fail",
                     message: " The migration couldn't be created.",
--- ProjectFinal/force-app/main/default/lwc/viewLogs/viewLogs.js
+++ ProjectFinal/force-app/main/default/lwc/viewLogs/viewLogs.js
@@ -3,12 +3,15 @@ import EXCEPTION_MSG_FIELD from '@salesforce/schema/CustomException__c.Exception
 import CLASS_NAME_FIELD from '@salesforce/schema/CustomException__c.ClassName__c';
 import METHOD_NAME_FIELD from '@salesforce/schema/CustomException__c.MethodName__c';
 import LINE_NUMBER_FIELD from '@salesforce/schema/CustomException__c.Line_Number__c';
+import DATE from '@salesforce/schema/CustomException__c.Date__c';
 import getLogs from '@salesforce/apex/LogsController.getLogs';
 const COLUMNS = [
-    { label: 'Exception Message', fieldName: EXCEPTION_MSG_FIELD.fieldApiName, type: 'text' },
+    { label: 'Message', fieldName: EXCEPTION_MSG_FIELD.fieldApiName, type: 'text' },
     { label: 'Class Name', fieldName: CLASS_NAME_FIELD.fieldApiName, type: 'text' },
     { label: 'Method Name', fieldName: METHOD_NAME_FIELD.fieldApiName, type: 'text' },
-    { label: 'Line Number', fieldName: LINE_NUMBER_FIELD.fieldApiName, type: 'text' }
+    { label: 'Line Number', fieldName: LINE_NUMBER_FIELD.fieldApiName, type: 'text' },
+    { label: 'Date', fieldName: DATE.fieldApiName, type: 'datetime' }
+    
 ];
 export default class ViewLogs extends LightningElement {
     columns = COLUMNS;
